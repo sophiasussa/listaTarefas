a0130231b037c9a034f127da74afb00d99480e01
sophiasussa <sophiasussa@outlook.com>
2024-07-03 15:33:57 -0300

    Commit teste

---
+++ src/main/java/com/example/application/model/Status.java
@@ -0,0 +1,19 @@
+package com.example.application.model;
+
+public class Status {
+    private int id;
+    private String descricao;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
